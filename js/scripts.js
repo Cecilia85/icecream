1d5934a15cb36123565701eccc2134ed1cb647ce
Cecilia Le Brech and Jenna Montero <cecilialebrech@gmail.com>
2016-06-13 20:09:10 -0700

    Make final add ons for lesson

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,14 @@ $(document).ready(function() {
     var favThings = [food, band, animal, book];
 
 
-    $("ul#display").append("<li>"+ favThings[0] + "</li>");
-    $("ul#display").append("<li>"+ favThings[1] + "</li>");
-    $("ul#display").append("<li>"+ favThings[2] + "</li>");
-    $("ul#display").append("<li>"+ favThings[3] + "</li>");
+    // $("ul#display").append("<li>"+ favThings[0] + "</li>");
+    // $("ul#display").append("<li>"+ favThings[1] + "</li>");
+    // $("ul#display").append("<li>"+ favThings[2] + "</li>");
+    // $("ul#display").append("<li>"+ favThings[3] + "</li>");
+
+    // var newArray = [favThings[1], favThings[0], favThings[2]];
+
+    $("ul#display").append("<li>" + favThings[1] +favThings[0]+favThings[2] +"</li>");
 
     event.preventDefault();
   });
